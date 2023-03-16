75a248bfcecad6a69fe2d224f62bcad74133d39c
413C5 <124840787+413C5@users.noreply.github.com>
2023-03-16 17:02:49 -0600

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmPrincipal
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.cmbSpider = New System.Windows.Forms.ComboBox()
+        Me.picSpider = New System.Windows.Forms.PictureBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.cmbDuende = New System.Windows.Forms.ComboBox()
+        Me.picDuende = New System.Windows.Forms.PictureBox()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.picSpider, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.picDuende, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.TabControl1.Location = New System.Drawing.Point(0, 0)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(611, 354)
+        Me.TabControl1.TabIndex = 0
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.cmbSpider)
+        Me.TabPage1.Controls.Add(Me.picSpider)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(603, 321)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Spider-Man"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'cmbSpider
+        '
+        Me.cmbSpider.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbSpider.FormattingEnabled = True
+        Me.cmbSpider.Items.AddRange(New Object() {"Sonriendo", "Llorando", "Enojado", "Burla"})
+        Me.cmbSpider.Location = New System.Drawing.Point(52, 74)
+        Me.cmbSpider.Name = "cmbSpider"
+        Me.cmbSpider.Size = New System.Drawing.Size(259, 28)
+        Me.cmbSpider.TabIndex = 1
+        '
+        'picSpider
+        '
+        Me.picSpider.Location = New System.Drawing.Point(341, 33)
+        Me.picSpider.Name = "picSpider"
+        Me.picSpider.Size = New System.Drawing.Size(229, 266)
+        Me.picSpider.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picSpider.TabIndex = 0
+        Me.picSpider.TabStop = False
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.cmbDuende)
+        Me.TabPage2.Controls.Add(Me.picDuende)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 29)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(603, 321)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Duende Verde"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'cmbDuende
+        '
+        Me.cmbDuende.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.cmbDuende.FormattingEnabled = True
+        Me.cmbDuende.Items.AddRange(New Object() {"Sonriendo", "Llorando", "Enojado", "Burla"})
+        Me.cmbDuende.Location = New System.Drawing.Point(52, 74)
+        Me.cmbDuende.Name = "cmbDuende"
+        Me.cmbDuende.Size = New System.Drawing.Size(259, 28)
+        Me.cmbDuende.TabIndex = 3
+        '
+        'picDuende
+        '
+        Me.picDuende.Location = New System.Drawing.Point(341, 33)
+        Me.picDuende.Name = "picDuende"
+        Me.picDuende.Size = New System.Drawing.Size(229, 266)
+        Me.picDuende.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picDuende.TabIndex = 2
+        Me.picDuende.TabStop = False
+        '
+        'frmPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(611, 354)
+        Me.Controls.Add(Me.TabControl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmPrincipal"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Spidey y Duende Verde"
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.picSpider, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.picDuende, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents cmbSpider As ComboBox
+    Friend WithEvents picSpider As PictureBox
+    Friend WithEvents cmbDuende As ComboBox
+    Friend WithEvents picDuende As PictureBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class frmPrincipal
+#Region "Atributos"
+    Private _indiceSpider As Integer
+    Private _indiceDuende As Integer
+    Public Property IndiceSpider As Integer
+        Set(value As Integer)
+            If value <> _indiceSpider Then                'Validar que haya algun cambio en el valor
+                _indiceSpider = value
+            End If
+        End Set
+        Get
+            Return _indiceSpider
+        End Get
+    End Property
+
+    Public Property IndiceDuende As Integer
+        Set(value As Integer)
+            If value <> _indiceDuende Then                'Validar que haya algun cambio en el valor
+                _indiceDuende = value
+            End If
+        End Set
+        Get
+            Return _indiceDuende
+        End Get
+    End Property
+#End Region
+#Region "Metodos"
+    Private Sub cambiarImagenSpider(ByVal indice)
+        Select Case indice
+            Case 0
+                picSpider.Image = My.Resources.spiderSonriendo
+            Case 1
+                picSpider.Image = My.Resources.spiderLlorando
+            Case 2
+                picSpider.Image = My.Resources.spiderEnojado
+            Case 3
+                picSpider.Image = My.Resources.spiderBurla
+        End Select
+    End Sub
+    Private Sub cambiarImagenDuende(ByVal indice)
+        Select Case indice
+            Case 0
+                picDuende.Image = My.Resources.duendeSonriendo
+            Case 1
+                picDuende.Image = My.Resources.duendeLlorando
+            Case 2
+                picDuende.Image = My.Resources.duendeEnojado
+            Case 3
+                picDuende.Image = My.Resources.duendeBurla
+        End Select
+    End Sub
+#End Region
+
+#Region "Eventos"
+    Private Sub frmPrincipal_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbSpider.SelectedIndex = 0
+        cmbDuende.SelectedIndex = 0
+        IndiceSpider = cmbSpider.SelectedIndex
+        IndiceDuende = cmbDuende.SelectedIndex
+    End Sub
+
+    Private Sub cmbSpider_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbSpider.SelectedIndexChanged
+        IndiceSpider = cmbSpider.SelectedIndex
+        cambiarImagenSpider(IndiceSpider)
+    End Sub
+
+    Private Sub cmbDuende_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbDuende.SelectedIndexChanged
+        IndiceDuende = cmbDuende.SelectedIndex
+        cambiarImagenDuende(IndiceDuende)
+    End Sub
+#End Region
+End Class
